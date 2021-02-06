98e0e207033d98ed5220f9baeb18c1df414c6ba8
jscfping <jscfping@gmail.com>
2021-02-06 10:29:11 +0800

    [TryAsync]非同步功能實驗庫初步建立

---
+++ TryAsync/TestAsync.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TryAsync
+{
+    public class TestAsync
+    {
+        public async Task ExecuteAsync()
+        {
+            Console.WriteLine($"[{DateTime.Now}] Start with no error:");
+            await Task.WhenAll(RunTaskAAsync(), RunTaskBAsync(), RunTaskCAsync(false));
+            Console.WriteLine($"[{DateTime.Now}] End");
+            Console.WriteLine($"[{DateTime.Now}] Start with error:");
+            await Task.WhenAll(RunTaskAAsync(), RunTaskBAsync(), RunTaskCAsync(true));
+            Console.WriteLine($"[{DateTime.Now}] End");
+            Console.ReadLine();
+        }
+
+        private async Task<string> RunTaskAsync(string taskName, int seconds)
+        {
+            await Task.Delay(seconds * 1000);
+            Console.WriteLine($"[{Thread.CurrentThread.ManagedThreadId}]{taskName}:{seconds}");
+            return taskName;
+        }
+        private async Task RunTaskAAsync()
+        {
+            await RunTaskAsync("A1", 5);
+        }
+        private async Task RunTaskBAsync()
+        {
+            await RunTaskAsync("B1", 1);
+            await RunTaskAsync("B2", 5);
+        }
+        private async Task RunTaskCAsync(bool hasError)
+        {
+            await RunTaskAsync("C1", 2);
+            try
+            {
+                if (hasError)
+                {
+                    await GetError();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"get error: {ex.Message}");
+                return;
+            }
+            await RunTaskAsync("C2", 1);
+            await RunTaskAsync("C3", 1);
+        }
+        private Task GetError()
+        {
+            throw new Exception("Get a error!");
+        }
+
+
+    }
+}
--- WebCore31/Program.cs
+++ WebCore31/Program.cs
@@ -6,14 +6,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TryAsync;
 
 namespace WebCore31
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var asyncTry = new TestAsync();
+            await asyncTry.ExecuteAsync();
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
