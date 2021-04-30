85b9db34ac7de5e5f9f5611c6b87c5d17687fa43
jscfping <jscfping@gmail.com>
2021-04-30 16:07:59 +0800

    Framework46更新為WindowsForm

--- Framework46/Program.cs
+++ Framework46/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Framework46
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
+            Console.WriteLine(nameof(Framework46));
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
--- Framework46/Properties/AssemblyInfo.cs
+++ Framework46/Properties/AssemblyInfo.cs
@@ -20,14 +20,14 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("a79cb6b6-23a5-41e5-8cc9-e34d4cb906f9")]
+[assembly: Guid("1c585336-68f4-4390-b56f-1bff60cf672a")]
 
-// 組件的版本資訊由下列四個值所組成:
+// 組件的版本資訊由下列四個值所組成: 
 //
 //      主要版本
 //      次要版本
 //      組建編號
-//      修訂
+//      修訂編號
 //
 // 您可以指定所有的值，也可以使用 '*' 將組建和修訂編號
 // 設為預設，如下所示:
