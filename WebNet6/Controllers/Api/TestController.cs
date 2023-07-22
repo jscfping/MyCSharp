db3d2810614ac982db8e077180aa9b118a11d9be
jscfping <jscfping@gmail.com>
2023-07-22 22:16:35 +0800

    feat: TestController add ILogger

--- WebNet6/Controllers/Api/TestController.cs
+++ WebNet6/Controllers/Api/TestController.cs
@@ -1,6 +1,7 @@
 using Core31.Library.Services.RabbitMQ;
 using Core31.Library.Services.Redis;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using WebNet6.Filters.Action;
 using WebNet6.Filters.Auth;
@@ -10,14 +11,17 @@ namespace WebNet6.Controllers.ApiDevelopment
     public class TestController : ApiControllerBase
     {
         private readonly AppSetting _appSetting;
-        public TestController(IOptions<AppSetting> appSettings)
+        private readonly ILogger _logger;
+        public TestController(IOptions<AppSetting> appSettings, ILogger<TestController> logger)
         {
             _appSetting = appSettings.Value;
+            _logger = logger;
         }
 
         [HttpGet("MyValue")]
         public string GetMyValue()
         {
+            _logger.LogInformation(_appSetting.MyValue);
             return _appSetting.MyValue;
         }
 
